35b9b9cd3647c338375ac8906588c632b35f65fe
Shubham Gulati <shubhamgulati91@gmail.com>
2023-01-17 21:50:05 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -83,10 +83,10 @@ confirm_action () {
 
 # Check if XCode Command line tools are installed
 if ! (type xcode-select >&- && xpath=$( xcode-select --print-path ) && test -d "${xpath}" && test -x "${xpath}") ; then
-    # echo "${RED}Need to install the XCode Command Line Tools (or XCode) first! Starting install...${NC}"
-    # # Install XCode Command Line Tools
-    # xcode-select --install &>/dev/null
-    # exit 1
+    echo "${RED}Need to install the XCode Command Line Tools (or XCode) first! Starting install...${NC}"
+    # Install XCode Command Line Tools
+    xcode-select --install &>/dev/null
+    exit 1
 fi
 
 case $SETUP_MODE in
